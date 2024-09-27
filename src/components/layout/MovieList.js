48144cb4e08a5aba5b5ba7501312bf9f11c18fce
tien <tienvn998@gmail.com>
2024-09-27 20:20:25 +0700

    finish moviesList

--- src/components/layout/MovieList.js
+++ src/components/layout/MovieList.js
@@ -5,8 +5,8 @@ import { generateImgUrl_Origin, generateImgUrl_W500 } from '../../ulti/http';
 export default function MovieList({ list, category, landScape = true }) {
 
     return (
-        // <div className='w-full bg-main text-white overflow-y-auto '>
-        <div className='w-full bg-main text-white px-10 pt-10 pb-16 overflow-clip'>
+        // <div className='w-full bg-main text-white px-10 pt-10 pb-16 overflow-y-clip '>
+        <div className='w-full bg-main text-white px-10 pt-10 pb-16 overflow-y-auto '>
             {category && <h4 className='font-semibold text-2xl -mx-5 mb-6'>{category}</h4>}
             <div className='flex gap-4 w-max'>
                 {
@@ -24,39 +24,49 @@ function MovieItem({ movie, isLandscape }) {
         'h-36 w-72 object-cover' :
         'h-64 w-44 object-cover'
     const { id = '', name = '', poster_path = '' } = movie
+    const { imgRef, imgSrc, blurClass } = useScrollToLoadImg(poster_path)
 
-    const [imgSrc, setImgSrc] = useState(generateImgUrl_W500(poster_path))
-    const [blurClass, setBlurClass] = useState(isLandscape ? ' blur' : '')
+    return (
+        <Link to={`/`}>
+            <img ref={imgRef} src={imgSrc} alt={name} className={`hover:scale-110 duration-150 ` + imgSizeClass + blurClass} />
+        </Link>
+    )
+}
 
+function useScrollToLoadImg(poster_path) {
     const imgRef = useRef()
+    const [imgSrc, setImgSrc] = useState(generateImgUrl_W500(poster_path))
+    const [blurClass, setBlurClass] = useState(' blur')
+
     useEffect(() => {
         const imgRectY = imgRef.current.getBoundingClientRect().top
 
-        if (window.scrollY >= imgRectY - (document.documentElement.clientHeight)) {
+        isIntersect() && loadImg()
+        window.addEventListener('scroll', event)
+
+
+        // functions
+        function event(e) {
+            isIntersect() && loadImg(event)
+        }
+        function isIntersect() {
+            if (window.scrollY >= imgRectY - (document.documentElement.clientHeight))
+                return true
+            return false
+        }
+        // pass event func reference to loadImg() to removeEventListener
+        function loadImg(event) {
             setImgSrc(generateImgUrl_Origin(poster_path))
 
             imgRef.current.addEventListener('load', e => {
                 setBlurClass('')
             })
-        }
 
-        window.addEventListener('scroll', event)
-        function event(e) {
-            if (window.scrollY >= imgRectY - (document.documentElement.clientHeight)) {
-                setImgSrc(generateImgUrl_Origin(poster_path))
-
-                imgRef.current.addEventListener('load', e => {
-                    setBlurClass('')
-                })
-
-                window.removeEventListener('scroll', event)
-            }
+            event && window.removeEventListener('scroll', event)
         }
     }, [])
 
-    return (
-        <Link to={`/`}>
-            <img ref={imgRef} src={imgSrc} alt={name} className={imgSizeClass + blurClass} />
-        </Link>
-    )
+    return {
+        imgRef, imgSrc, blurClass
+    }
 }
